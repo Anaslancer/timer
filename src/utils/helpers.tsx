105fc8b0f3ff146e4f79c5a62142fb4f1aa6ff9e
Anaslancer <anasup1995@gmail.com>
2024-11-27 07:10:27 -0500

    remove unnecessary codes

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,8 +1,3 @@
-// Add helpers here. This is usually code that is just JS and not React code. Example: write a function that
-// calculates number of minutes when passed in seconds. Things of this nature that you don't want to copy/paste
-// everywhere.
-
-import { useState, useEffect } from 'react';
 import { Timer } from './context';
 import CONST from './CONST';
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -10,38 +10,6 @@ const StyledQueueContainer = styled.div`
   margin-top: 40px;
 `;
 
-/*
-const TimersContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 60vh;
-  background-color: #f5f5f5;
-  gap: 20px;
-  width: 100%;
-  margin: 0 auto;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  padding-top: 20px;
-  padding-bottom: 20px;
-`;
-
-
-const TimerDisplay = styled.div`
-  width: 340px;
-  height: 340px;
-  background-color: #e0e0e0;
-  border: 2px solid #ccc;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.5rem;
-  font-weight: bold;
-  color: #333;
-  margin-bottom: 20px;
-`;
-*/
 const Home = () => {
   const { time, isRunning, timersQueue, startQueue, stopQueue, resetQueue } = useTimerContext();
 
